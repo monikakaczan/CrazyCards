1db12e81b7115316e3b200cae164472f8c5c31a2
Student <kaczanowskamonika@gmail.com>
2019-08-22 18:34:25 +0100

    set up the user form

--- src/form.js
+++ src/form.js
@@ -2,6 +2,26 @@ import React, {Component} from 'react'
 
 class Form extends Component {
 
-  
+  render(){
+    return(
+      <div>
+      <label>Emoployment type</label>
+           <select>
+            <option value = "part-time">Part-time</option>
+            <option value = "full-time">Full-time</option>
+            <option value = "student">Student</option>
+          </select>
+
+          <div>
+          <label>Annual income</label>
+          <select>
+            <option value = "lowerIncome">less than £16,000</option>
+            <option value = "higherIncome">more than £16,000</option>
+            </select>
+        </div>
+        </div>
+    )
+  }
+
 }
 export default Form
