758e7af9278dd95c5c7f8f2fa52fa0a778a928b2
Student <kaczanowskamonika@gmail.com>
2019-08-23 18:29:27 +0100

    apply css styling

--- src/cardList.js
+++ src/cardList.js
@@ -2,22 +2,22 @@ export const cardList = [
   {
     id: 0,
     name: "Student Life",
+    annualIncome: ["lowerIncome"],
     employmentType: ["student"],
-    annualIncome: ["less than £16,000"],
     avaliableCredit: 1200
   },
   {
     id: 1,
     name: "Anywhere Card",
+    annualIncome: ["higherIncome"],
     employmentType: ["student", "part-time", "full-time"],
-    annualIncome: ["more than £16,000"],
     avaliableCredit: 300
   },
   {
     id: 2,
     name: "Liquid Card",
+    annualIncome: ["higherIncome"],
     employmentType: ["student", "part-time", "full-time"],
-    annualIncome: ["more than £16,000"],
     avaliableCredit: 3000
   }
 ];
